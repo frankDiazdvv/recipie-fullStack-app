0aef4cf7968d57692029e237ce059158264bc961
frankDiazdvv <158342906+frankDiazdvv@users.noreply.github.com>
2025-02-01 17:04:13 -0500

    Create Server and connect to DB

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+//middleware
+app.use(cors()); //allows backend to get requests from different domains.
+app.use(express.json()); //allows incomming JSON req.body to become a JS object.
+
+//Connection to mongoDB
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected!")).catch(
+    err => console.error("Connection to MongoDB failed!", err)
+);
+
+//Test route
+app.get('/', (req,res) => {
+    res.status(200);
+    res.send('Backend Running...');
+});
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
